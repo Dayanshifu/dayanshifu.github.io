9fc526cf39d378398cf10a95e53c20131bff5fc6
little颜 <dayanshifu@outlook.com>
2024-08-10 13:16:49 +0800

    Update text.js

--- js/text.js
+++ js/text.js
@@ -23,9 +23,9 @@ In [4]: Dayanshifu.getLinks()\n\
 Out [4]: {'感谢你们的支持！':'site',\n\
           '<img src='https://glacier-studio.github.io/res/icon/icon.svg' height='24px' width='24px'>冰川工作室':<a id='l4' target='_blank' href='https://glacier-studio.github.io/'>,\n\
           '<img src='https://static.codemao.cn/pickduck/SJyHSSkh6.png' height='24px' width='24px'>SLIGHTNING':<a id='l5' target='_blank' href='https://slightning.rechen.xyz/'>,\n\
-          '<img src='https://zmh-program.site/avatar/zmh-program.webp' height='24px' width='24px'>zmh-program': <a id='l6' target='_blank' href='https://zmh-program.site/'>,\n\
+          '<img src='https://zmh-program.site/avatar.webp' height='24px' width='24px'>zmh-program': <a id='l6' target='_blank' href='https://zmh-program.site/'>,\n\
           '<img src='https://blog.lolicon.best/img/favicon.webp' height='24px' width='24px'>半昭の小站': <a id='l7' target='_blank' href='https://blog.lolicon.best/'>,\n\
-          '<img src='https://static.codemao.cn/pickduck/SkmQEAE2T.jpg' height='24px' width='24px'>猫鱼a 软软弹弹的': '<a id='l12' target='_blank' href='https://zybqw.github.io/'>'\n\
+          '<img src='https://static.codemao.cn/pickduck/SkmQEAE2T.jpg' height='24px' width='24px'>猫鱼a': '<a id='l12' target='_blank' href='https://zybqw.github.io/'>'\n\
           }\n\
 In [5]: getCountdown2zhongkao(Dayanshifu)\n\
 Out [5]: '<span id='countdown'>'\n\
