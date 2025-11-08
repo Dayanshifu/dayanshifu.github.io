156f9807be0bbc4d895227305d4a65121fe0b6e6
Yan Xueqian <dayanshifu@outlook.com>
2025-11-08 13:40:51 +0800

    1

--- js/text.js
+++ js/text.js
@@ -8,18 +8,18 @@ In [1]:  let Dayanshifu = EntityFactory.getEntity('Dayanshifu')\n\
           I'm Dayanshifu\n\
 In [2]:  Dayanshifu.Site()\n\
 Out [2]: {'website':<a id='l1' href='https://lolicon.xin'>,\n\
-          '<image src='https://www.bilibili.com/favicon.ico'width='16px' height='16px'/>bilibili': <a id='l9' target='_blank' href='https://space.bilibili.com/593963058'>,\n\
-          '<image src='https://static.codemao.cn/coco/player/unstable/B1F3qc2Hj.image/svg+xml'width='16px' height='16px'/>codemao': <a id='l10' target='_blank' href='https://shequ.codemao.cn/user/3408348'>,\n\
-          '<image src='https://cdn-community.bcmcdn.com/47/community/d2ViXzMwMDFfMzQwODM0OF8wXzE3NjI1Nzk4MjQ4NzhfYTc5ZTY1MjI.png'width='16px' height='16px'/>github': <a id='l11' target='_blank' href='https://github.com/dayanshifu'>\n\
+          '<image src='https://www.bilibili.com/favicon.ico'width='16px' height='16px'/>bilibili': '<a id='l9' target='_blank' href='https://space.bilibili.com/593963058'>',\n\
+          '<image src='https://static.codemao.cn/coco/player/unstable/B1F3qc2Hj.image/svg+xml'width='16px' height='16px'/>codemao': '<a id='l10' target='_blank' href='https://shequ.codemao.cn/user/3408348'>',\n\
+          '<image src='https://cdn-community.bcmcdn.com/47/community/d2ViXzMwMDFfMzQwODM0OF8wXzE3NjI1Nzk4MjQ4NzhfYTc5ZTY1MjI.png'width='16px' height='16px'/>github': '<a id='l11' target='_blank' href='https://github.com/dayanshifu'>'\n\
           }\n\
 In [3]:  Dayanshifu.getWorks()\n\
-Out [3]: {'Home': <a id='l3' target='_blank' href='https://home.lolicon.xin'>}\n\
+Out [3]: {'Home': '<a id='l3' target='_blank' href='https://home.lolicon.xin'>'}\n\
 In [4]:  Dayanshifu.getLinks()\n\
 Out [4]: {'感谢你们的支持！': 'site',\n\
-          '<img src='https://static.codemao.cn/pickduck/SkmQEAE2T.jpg' height='24px' width='24px'>猫鱼a': '<a id='l12' target='_blank' href='https://aurzex.github.io/'>'\n\
-          '<img src='https://static.codemao.cn/pickduck/S1Y8l__iJg.jpg' height='24px' width='24px'>ChenServer': '<a id='l13' target='_blank' href='https://etaris.moe/'>'\n\
-          '<img src='https://cravatar.com/avatar/0fc2c5f90ea8afbfb604182b74a423ce?d=mp&s=240' height='24px' width='24px'>你好李鑫': '<a id='l14' target='_blank' href='https://1zdigital.space/'>'\n\
-          '<img src='https://cdn-community.bcmcdn.com/47/community/d2ViXzMwMDFfMzQwODM0OF8wXzE3NjI1MzUxMDI0MjFfNjUyYjBjNWE.jpeg' height='24px' width='24px'>HMZ': '<a id='l6' target='_blank' href='https://hmz168.lolicon.xin/'>'\n\
+          '<img src='https://static.codemao.cn/pickduck/SkmQEAE2T.jpg' height='24px' width='24px'>猫鱼a': '<a id='l12' target='_blank' href='https://aurzex.github.io/'>',\n\
+          '<img src='https://static.codemao.cn/pickduck/S1Y8l__iJg.jpg' height='24px' width='24px'>ChenServer': '<a id='l13' target='_blank' href='https://etaris.moe/'>',\n\
+          '<img src='https://cravatar.com/avatar/0fc2c5f90ea8afbfb604182b74a423ce?d=mp&s=240' height='24px' width='24px'>你好李鑫': '<a id='l14' target='_blank' href='https://1zdigital.space/'>',\n\
+          '<img src='https://cdn-community.bcmcdn.com/47/community/d2ViXzMwMDFfMzQwODM0OF8wXzE3NjI1MzUxMDI0MjFfNjUyYjBjNWE.jpeg' height='24px' width='24px'>HMZ': '<a id='l6' target='_blank' href='https://hmz168.lolicon.xin/'>',\n\
           '<img src='https://static.codemao.cn/pickduck/rJzpT7WUxe.ico' height='24px' width='24px'>数码空间': '<a id='l7' target='_blank' href='https://hello.1zdigital.space/'>'\n\
           }\n\
 In [5]:  Dayanshifu.getInfo()\n\
@@ -30,39 +30,39 @@ var 输出区 = document.getElementById("input");
 function 给标签上内容() {
     try {
         var l1 = document.getElementById("l1");
-        l1.innerHTML = "'https://lolicon.xin'";
+        l1.innerHTML = "https://lolicon.xin";
     } catch { }
     try {
         var l9 = document.getElementById("l9");
-        l9.innerHTML = "'https://space.bilibili.com/593963058'";
+        l9.innerHTML = "https://space.bilibili.com/593963058";
     } catch { }
     try {
         var l10 = document.getElementById("l10");
-        l10.innerHTML = "'https://shequ.codemao.cn/user/3408348'";
+        l10.innerHTML = "https://shequ.codemao.cn/user/3408348";
     } catch { }
     try {
         var l11 = document.getElementById("l11");
-        l11.innerHTML = "'https://github.com/dayanshifu'";
+        l11.innerHTML = "https://github.com/dayanshifu";
     } catch { }
     try {
         var l3 = document.getElementById("l3");
-        l3.innerHTML = "'https://home.lolicon.xin'";
+        l3.innerHTML = "https://home.lolicon.xin";
     } catch { }
     try {
         var l4 = document.getElementById("l4");
-        l4.innerHTML = "'https://glacier-studio.github.io/'";
+        l4.innerHTML = "https://glacier-studio.github.io/";
     } catch { }
     try {
         var l5 = document.getElementById("l5");
-        l5.innerHTML = "'https://slightning.rechen.xyz/'";
+        l5.innerHTML = "https://slightning.rechen.xyz/";
     } catch { }
     try {
         var l6 = document.getElementById("l6");
-        l6.innerHTML = "'https://hmz168.lolicon.xin/'";
+        l6.innerHTML = "https://hmz168.lolicon.xin/";
     } catch { }
     try {
         var l7 = document.getElementById("l7");
-        l7.innerHTML = "'https://hello.1zdigital.space/'";
+        l7.innerHTML = "https://hello.1zdigital.space/";
     } catch { }
     try {
         var l8 = document.getElementById("l8");
